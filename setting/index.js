cf54652b40e0e7483eb74b936389003ba52cd05c
kNo <linsms@gmail.com>
2025-08-03 10:33:37 +0200

    settings state management

--- setting/index.js
+++ setting/index.js
@@ -1,35 +1,98 @@
+import Styles from "./styles";
 
 AppSettingsPage({
   // 1. Define state
   state: {
-    testKey: null
+    testKey: null,
+    config: {
+      URL: "zzz",
+      token: ""
+    }
+  },
+  setState(props) {
+    this.state.props = props;
+    console.log("config: ", this.state.config);
+  },
+  setItem() {
+    const newString = JSON.stringify(this.state.config);
+    this.state.settingsStorage.setItem("config", newString);
+  },
+  saveToConfig(key, value) {
+    this.state.config[key] = value;
+    this.setItem();
   },
   build(props) {
     // 2. Get SettingsStorage
-    this.getStorage(props)
+    this.getStorage(props);
 
     // 3. Logic
     const toggleButtonMap = {
-      ['Hello Zepp OS']: 'Hello World',
-      ['Hello World']: 'Hello Zepp OS'
-    }
+      ["Hello Zepp OSS"]: "Hello Worlda",
+      ["Hello Worlda"]: "Hello Zepp OSS"
+    };
 
     // 4. Return Render Function
-    return Button({
-      label: this.state.testKey,
-      style: {
-        fontSize: '12px',
-        borderRadius: '30px',
-        background: '#D85E33',
-        color: 'white'
+    return View(
+      {
+        style: {
+          padding: "12px 20px"
+        }
       },
-      onClick: () => {
-        // 5. Modify the data in settingsStorage in the callback function of the event
-        props.settingsStorage.setItem('testKey', toggleButtonMap[this.state.testKey])
-      }
-    })
+      [
+        Section(
+          {
+            title: "Configuration"
+          },
+          [
+            TextInput({
+              label: "HA URL",
+              bold: true,
+              value: this.state.config.URL,
+              placeholder: "Enter your Home Assistant URL",
+              subStyle: Styles.inputStyle,
+              maxLength: 200,
+              onChange: (value) => {
+                this.saveToConfig("URL", value);
+              }
+            }),
+            TextInput({
+              label: "Token",
+              bold: true,
+              value: this.state.config.token,
+              placeholder: "Enter your Home Assistant Token",
+              subStyle: Styles.inputStyle,
+              maxLength: 200,
+              onChange: (value) => {
+                this.saveToConfig("token", value);
+              }
+            })
+          ]
+        ),
+        Button({
+          label: this.state.testKey,
+          style: {
+            fontSize: "12px",
+            borderRadius: "30px",
+            background: "#D85E33",
+            color: "white"
+          },
+          onClick: () => {
+            // 5. Modify the data in settingsStorage in the callback function of the event
+            // props.settingsStorage.setItem(
+            //   "testKey",
+            //   toggleButtonMap[this.state.testKey]
+            // );
+          }
+        })
+      ]
+    );
   },
   getStorage(props) {
-    this.state.testKey = props.settingsStorage.getItem('testKey') || 'Hello World'
+    this.state.settingsStorage = props.settingsStorage;
+    this.state.testKey = props.settings.testKey || "Hello World";
+    this.state.config = JSON.parse(props.settings.config) || {
+      URL: "",
+      Token: ""
+    };
   }
-})
+});
---
+++ setting/styles.js
@@ -0,0 +1,11 @@
+export default {
+  inputStyle: {
+    color: "#333",
+    fontSize: "11px",
+    border: "1px solid #ccc",
+    marginBottom: "10px",
+    height: "20px",
+    display: "flex",
+    alignItems: "center"
+  }
+};
